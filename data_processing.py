71f8e73602f00ea02070d3a996646e2da4f90172
sofia <sofia.gilardini@gmail.com>
2024-09-15 18:38:27 +0100

    trainval data proc

--- data_processing.py
+++ data_processing.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import aux_functions as auxf
 import os
+from sklearn.model_selection import train_test_split
 
 
 """
@@ -39,3 +40,38 @@ for user in user_list:
         # check to make sure dimensions are correct:
         if not emg.shape[0] == glove.shape[0]:
             raise ValueError("EMG and Glove data shape not aligned.")
+        
+
+# join dataset 1 and dataset 2 into trainval, then split randomly 80/20 training and validation. (they are datasets from different days)
+
+
+for user in user_list: 
+        
+
+        dataset = 1
+
+        emg_d1 = auxf.getProcessedData(user = user, dataset=dataset, mode='emg')
+        glove_d1 = auxf.getProcessedData(user = user, dataset=dataset, mode='glove')
+
+        dataset = 2
+
+        emg_d2 = auxf.getProcessedData(user = user, dataset=dataset, mode='emg')
+        glove_d2 = auxf.getProcessedData(user = user, dataset=dataset, mode='glove')
+
+        emg_trainval = np.concatenate((emg_d1, emg_d2))
+        glove_trainval = np.concatenate((glove_d1, glove_d2))
+
+        emg_train, emg_val, glove_train, glove_val = train_test_split(emg_trainval, glove_trainval, test_size = 0.2, random_state=42)
+
+        train_path = './datasets/train'
+        auxf.ensure_directory_exists(train_path)
+        val_path = './datasets/val'
+        auxf.ensure_directory_exists(val_path)
+
+        np.save(f'{train_path}/user{user}_emg_train.npy', emg_train)
+        np.save(f'{val_path}/user{user}_emg_val.npy', emg_val)
+        np.save(f'{train_path}/user{user}_glove_train.npy', glove_train)
+        np.save(f'{val_path}/user{user}_glove_val.npy', glove_val)
+
+
+
